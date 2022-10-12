b110dce9aa2c90b9763ea45db154af659ec7c3ba
codywashbrook <codywashbrook@hotmail.com>
2022-10-12 14:17:35 -0300

    Initial commit

---
+++ Conditionals3/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Conditionals3
+{
+    internal class Program
+    {
+        static int health;
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Conditionals 3");
+            Console.WriteLine();
+
+            health = 100;
+            Console.WriteLine("Health: " + health);
+            if (health > 0)
+            {
+                Console.WriteLine("Alive");
+            }
+            else
+            {
+                Console.WriteLine("Dead");
+            }
+            TakeDamage(100);
+            Console.WriteLine("Health: " + health);
+            if (health > 0)
+            {
+                Console.WriteLine("Alive");
+            }
+            else
+            {
+                Console.WriteLine("Dead");
+            }
+            Console.ReadKey(true);
+        }
+
+        static void TakeDamage(int damage)
+        {
+            Console.WriteLine("Player is about to take " + damage + " damage...");
+            health = health - damage;
+        }
+    }
+}
